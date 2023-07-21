3effaeca677b471b8e84662b0634ecd7482e7c48
nextdarius <pintidarius@gmail.com>
2023-07-21 08:31:45 +0300

    Few modifications for 2023 II

---
+++ 3/1.js
@@ -0,0 +1,8 @@
+function add(x, y) {
+  return x + y;
+}
+
+add(5, 'something');
+
+let a = 1;
+a = 'test';
---
+++ 3/1.ts
@@ -0,0 +1,12 @@
+function add(x: number, y: number) {
+  return x + y;
+}
+
+add(5, 'something');
+
+let a = 1;
+a = 'test';
+
+a == 'test';
+
+export {};
